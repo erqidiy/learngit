26d4ab200b652c5ec8bc50099b2e6807feaa53e6
erqitao <erqitao@qq.com>
2019-04-13 16:31:23 +0800

    test new command

---
+++ info.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# description: test Git commands
+# date: 2019-04-13
+# author: erqitao
+# email: erqitao@qq.com
+
+print('Hello There!")
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# author: erqitao
+# email: erqitao@qq.com
 
 for i in range(5):
     print('Hello World!')
